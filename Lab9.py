2170ba1e1204061db161a26ba3932fbfdd9f67ad
wilsonsong1 <G00418330@atu.ie>
2024-12-02 20:57:15 +0000

    Plotting corner Harris corners detection

--- Lab9.py
+++ Lab9.py
@@ -16,7 +16,13 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Harris corner detection
 dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
 
+threshold = 0.1; #number between 0 and 1
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(0, 0, 255),-1)
+
 #displaying the grayscale image
-cv2.imshow('grayscale', gray_image)
+cv2.imshow('grayscale', imgHarris)
 
 cv2.waitKey(0)
