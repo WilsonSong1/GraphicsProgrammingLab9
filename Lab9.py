9ae2916ee06a83dea453d48ce1ffbc9cc722906c
wilsonsong1 <G00418330@atu.ie>
2024-12-02 20:40:16 +0000

    Harris corner detection

--- Lab9.py
+++ Lab9.py
@@ -9,7 +9,10 @@ img = cv2.imread('ATU1.jpg',)
 #converted the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+#Harris corner detection
+dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
+
 #displaying the grayscale image
-cv2.imshow('Grayscale', gray_image)
+cv2.imshow('Grayscale', dst)
 
 cv2.waitKey(0)
