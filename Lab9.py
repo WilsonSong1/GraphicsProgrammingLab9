b8362e4ee964da39d8769b9057fc5fad161d2ee8
wilsonsong1 <G00418330@atu.ie>
2024-12-02 20:37:37 +0000

    Converted image to grayscale and output image

--- Lab9.py
+++ Lab9.py
@@ -5,3 +5,11 @@ from matplotlib import pyplot as plt
 
 #imported ATU jpeg using cv2
 img = cv2.imread('ATU1.jpg',)
+
+#converted the image to grayscale
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#displaying the grayscale image
+cv2.imshow('Grayscale', gray_image)
+
+cv2.waitKey(0)
