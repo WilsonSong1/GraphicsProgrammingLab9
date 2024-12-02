aa4cef463ba7104c3f19cce885f52a83f5af83c5
wilsonsong1 <G00418330@atu.ie>
2024-12-02 21:08:01 +0000

    Shi Tomasi corner detection plotted

--- Lab9.py
+++ Lab9.py
@@ -7,11 +7,12 @@ from matplotlib import pyplot as plt
 #imported ATU jpeg using cv2
 img = cv2.imread('ATU1.jpg',)
 
-#deep copy
-imgHarris = copy.deepcopy(img)
-
 #converted the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#deep copy 
+imgHarris = copy.deepcopy(img)
+imgShiTomasi = copy.deepcopy(img)
 
 #Harris corner detection
 dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
@@ -22,7 +23,28 @@ for i in range(len(dst)):
         if dst[i][j] > (threshold*dst.max()):
             cv2.circle(imgHarris,(j,i),3,(0, 0, 255),-1)
 
-#displaying the grayscale image
-cv2.imshow('grayscale', imgHarris)
+#Shi Tomasi corner detection algorithm
+corners = cv2.goodFeaturesToTrack(gray_image,10 , 0.01, 10)
+corners = np.int0(corners) #convert corners values to integer
 
-cv2.waitKey(0)
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(imgShiTomasi,(x,y),3,(0, 0, 255),-1)
+
+#Plotting the gray scaled image
+plt.subplot(2, 2, 1),plt.imshow(gray_image, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+
+#HarrisCorners
+plt.subplot(2, 2, 2),plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('HarrisCorners'), plt.xticks([]), plt.yticks([])
+
+#Shi Tomasi Corners
+plt.subplot(2, 2, 3),plt.imshow(cv2.cvtColor(imgShiTomasi, cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('ShiTomasi'), plt.xticks([]), plt.yticks([])
+
+#displaying the image
+plt.show()
+
+cv2.waitKey(0)  
+cv2.destroyAllWindows()
