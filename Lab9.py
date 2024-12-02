519e6b440a9ea4c40c27d0d42e9c82fd0cff1ecf
wilsonsong1 <G00418330@atu.ie>
2024-12-02 20:53:01 +0000

    Deep copy created

--- Lab9.py
+++ Lab9.py
@@ -1,10 +1,14 @@
 #Import cv2 nump and matplotlib
+import copy
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 #imported ATU jpeg using cv2
 img = cv2.imread('ATU1.jpg',)
+
+#deep copy
+imgHarris = copy.deepcopy(img)
 
 #converted the image to grayscale
 gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -13,6 +17,6 @@ gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 dst = cv2.cornerHarris(gray_image, 2, 3, 0.04)
 
 #displaying the grayscale image
-cv2.imshow('Grayscale', dst)
+cv2.imshow('grayscale', gray_image)
 
 cv2.waitKey(0)
